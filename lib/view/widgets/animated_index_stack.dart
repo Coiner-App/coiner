9289e6a2c4be148dd6513acc871cedce5c570909
ender1324 <ender1324@abv.bg>
2026-02-05 22:58:29 +0200

    Switch to a sliding animation
    LOTS OF NAVIGATOR FUCKERY. We can't have 2 navigators at once because they conflict, so we have to hack our way through by having 2 navigators on screen only WHILE the animation is running. Doesn't seem to hurt performance that much, also possible to implement gestures in this way so we'll see.

--- lib/view/widgets/animated_index_stack.dart
+++ lib/view/widgets/animated_index_stack.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class AnimatedIndexedStack extends StatefulWidget {
   final int index;
   final List<Widget> children;
+  final Duration duration;
 
   const AnimatedIndexedStack({
     super.key,
     required this.index,
     required this.children,
+    this.duration = const Duration(milliseconds: 400),
   });
 
   @override
@@ -16,62 +18,76 @@ class AnimatedIndexedStack extends StatefulWidget {
 
 class AnimatedIndexedStackState extends State<AnimatedIndexedStack>
     with SingleTickerProviderStateMixin {
+  
   late AnimationController _controller;
-  late Animation<double> _animation;
-  late int _index;
+  late int _currentIndex;
+  late int _previousIndex;
+  bool _ongoing = false;
 
   @override
   void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 150),
-    );
-    _animation = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.ease,
-      ),
-    );
-
-    _index = widget.index;
-    _controller.forward();
     super.initState();
+    _currentIndex = widget.index;
+    _previousIndex = widget.index;
+
+    _controller = AnimationController(vsync: this, duration: widget.duration);
   }
 
   @override
   void didUpdateWidget(AnimatedIndexedStack oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.index != _index) {
-      _controller.reverse().then((_) {
-        setState(() => _index = widget.index);
-        _controller.forward();
-      });
-    }
+
+    if (widget.index == _currentIndex) return;
+
+    setState(() {
+      _previousIndex = _currentIndex;
+      _currentIndex = widget.index;
+      _ongoing = true;
+    });
+
+    _controller.forward(from: 0).then((_) => setState(() {
+      _ongoing = false;
+    }));
   }
 
   @override
   void dispose() {
+    _ongoing = false; // Just in case
     _controller.dispose();
     super.dispose();
   }
 
+  Offset _incomingBegin(bool forward) => forward ? const Offset(1, 0) : const Offset(-1, 0);
+  Offset _outgoingEnd(bool forward) => forward ? const Offset(-1, 0) : const Offset(1, 0);
+
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Opacity(
-          opacity: _controller.value,
-          child: Transform.scale(
-            scale: 1.015 - (_controller.value * 0.015),
-            child: child,
-          ),
-        );
-      },
-      child: IndexedStack(
-        index: _index,
-        children: widget.children,
-      ),
+    final isForward = _currentIndex > _previousIndex;
+    final animation = CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic);
+    
+    if (!_ongoing) return widget.children[_currentIndex]; // One navigator at a time otherwise router goes insane
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        // outgoing
+        SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset.zero,
+            end: _outgoingEnd(isForward),
+          ).animate(animation),
+          child: widget.children[_previousIndex],
+        ),
+
+        // incoming
+        SlideTransition(
+          position: Tween<Offset>(
+            begin: _incomingBegin(isForward),
+            end: Offset.zero,
+          ).animate(animation),
+          child: widget.children[_currentIndex],
+        ),
+      ],
     );
   }
 }
